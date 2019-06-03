a503ed021177d94e37f2806a096433c52ab849d4
mohammed <me3dun@gmail.com>
2019-06-02 17:29:06 -0700

    all

---
+++ floating_placeholder_text_focus/1.js
@@ -0,0 +1,10 @@
+$('input').focus(function(){
+    $(this).parent().addClass('active');
+    $('input').focusout(function(){
+        if ($(this).val() == ''){
+            $(this).parent().removeClass('active');
+        }else{
+            $(this).parent().addClass('active');
+        }
+    })
+})
