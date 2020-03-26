b48121facffb2098af6829fda99e33ee4e870d5f
eddie0mohammed <me3dun@gmail.com>
2020-03-26 13:13:16 +0400

    new anim

---
+++ new/tabs_vanillajs/app.js
@@ -0,0 +1,23 @@
+
+
+
+const iconBx = document.querySelectorAll('.iconBx');
+const contentBx = document.querySelectorAll('.contentBx');
+
+
+
+
+for (let i = 0; i < iconBx.length; i++){
+    iconBx[i].addEventListener('mouseover', function(){
+        for (let i = 0; i < contentBx.length; i++){
+            contentBx[i].className = 'contentBx';
+        }
+        document.getElementById(this.dataset.id).className = 'contentBx active';
+
+        for (let i = 0; i < iconBx.length; i++){
+            iconBx[i].className = 'iconBx';
+        }
+
+        this.className = 'iconBx active';
+    })
+}
